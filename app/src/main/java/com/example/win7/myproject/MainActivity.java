8ff3ecd26c4e61a74deee1546e37da56856dd735
Harika <harikacherry1997@gmail.com>
2018-11-12 15:47:28 +0530

    Third commit

--- app/src/main/java/com/example/win7/myproject/MainActivity.java
+++ app/src/main/java/com/example/win7/myproject/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.win7.myproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText1,editText2;
+    private Button button1,button2;
+    private TextView textView1,textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText1=(EditText)findViewById(R.id.email);//email edittext
+        editText2=(EditText)findViewById(R.id.password);//password edittext
+        button1=(Button)findViewById(R.id.login);//login button
+        button2=(Button)findViewById(R.id.register);//register button
+        textView1=(TextView)findViewById(R.id.forgotpassword);//forgotpassword
+        textView2=(TextView)findViewById(R.id.Skip);//skip,ill do it later
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,NavigationActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
